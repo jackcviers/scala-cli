b50e35a6e033022d902a078ff7e762a46f76ddf8
Mark Hammons <markehammons@gmail.com>
2022-08-23 14:04:46 +0200

    Generalize the checks, begin checksum support

--- modules/cli/src/main/scala/scala/cli/commands/Doctor.scala
+++ modules/cli/src/main/scala/scala/cli/commands/Doctor.scala
@@ -7,6 +7,10 @@ import scala.cli.ScalaCli
 import scala.cli.internal.ProcUtil
 import scala.cli.signing.shared.Secret
 import scala.cli.commands.util.ScalaCliSttpBackend
+import scala.cli.commands.util.CommonOps._
+import scala.util.chaining._
+import sttp.model.Uri
+import java.security.MessageDigest
 
 // current version / latest version + potentially information that
 // scala-cli should be updated (and that should take SNAPSHOT version
@@ -87,56 +91,72 @@ object Doctor extends ScalaCommand[DoctorOptions] {
     // TODO
   }
 
+  private def checkAccessToNexusRepository(name: String)(
+    basicDir: Uri,
+    pom: Uri,
+    jar: Uri,
+    pomMd5: String,
+    jarMd5: String
+  )(backend: ScalaCliSttpBackend) = {
+    import sttp.client3._
+    import scala.util.Try
+
+    def runRequest(request: Request[Either[String, String], Any]) =
+      Try(request.send(backend)).toEither.left.map(_.getMessage())
+
+    val md = MessageDigest.getInstance("SHA1")
+    val responseBasicDir = runRequest(
+      basicRequest.get(basicDir)
+    )
+
+    val responsePom = runRequest(
+      basicRequest.get(pom)
+    )
+
+    val responseJar = runRequest(
+      basicRequest.get(jar)
+    )
+
+    val checks = List(
+      "general access" -> responseBasicDir.map(_.code.code == 200).getOrElse(false),
+      "jar download"   -> responseJar.map(_.code.code == 200).getOrElse(false),
+      "pom download"   -> responsePom.map(_.code.code == 200).getOrElse(false),
+      "not proxy blocked" -> responseBasicDir.map { response =>
+        (response.code.code != 403 && response.code.code != 504) || !response.headers.exists(
+          header => header.name == "Proxy-Construction" && header.value == "Keep-Alive"
+        )
+      }.getOrElse(false),
+      "jars are valid" -> responseJar.flatMap(_.body).map(_.getBytes()).map(md.digest).map(String(
+        _,
+        "UTF-8"
+      )).exists(_ == jarMd5)
+    )
+
+    println(s"$name:")
+    checks.map {
+      case (prompt, true)  => s"$prompt: O"
+      case (prompt, false) => s"$prompt: X"
+    }.mkString("\t", "\n\t", "").tap(println)
+  }
+
   // checkBinaryOrMainClass()??
 
   private def checkAccessToMavenOrGithub(): Unit = {
     // todo: maybe move to top?
-    import scala.cli.commands.util.CommonOps._
-    import scala.util.chaining._
     val loggingOptions = LoggingOptions()
     val logger         = loggingOptions.logger
 
     val backend = ScalaCliSttpBackend.httpURLConnection(logger)
-
-    import sttp.client3.basicRequest
     import sttp.client3._
 
-    try {
-      val responseMvnCentralDir = basicRequest.get(
-        uri"https://repo1.maven.org/maven2/ch/epfl/scala/library-example_2.13/1.0.1/"
-      ).send(backend)
+    checkAccessToNexusRepository("Maven Central")(
+      uri"https://repo1.maven.org/maven2/ch/epfl/scala/library-example_2.13/1.0.1/",
+      uri"https://repo1.maven.org/maven2/ch/epfl/scala/library-example_2.13/1.0.1/library-example_2.13-1.0.1.pom",
+      uri"https://repo1.maven.org/maven2/ch/epfl/scala/library-example_2.13/1.0.1/library-example_2.13-1.0.1.jar",
+      "",
+      ""
+    )(backend)
 
-      val responseMvnCentralPom = basicRequest.get(
-        uri"https://repo1.maven.org/maven2/ch/epfl/scala/library-example_2.13/1.0.1/library-example_2.13-1.0.1.pom"
-      ).send(backend)
-
-      val responseMvnCentralJar = basicRequest.get(
-        uri"https://repo1.maven.org/maven2/ch/epfl/scala/library-example_2.13/1.0.1/library-example_2.13-1.0.1.jar"
-      ).send(backend)
-
-      val checks = List(
-        "mvncentral access"  -> (responseMvnCentralDir.code.code != 200),
-        "mvn central jar dl" -> (responseMvnCentralJar.code.code != 200),
-        "mvncentral pom dl"  -> (responseMvnCentralPom.code.code != 200),
-        "mvn central access proxied" -> ((responseMvnCentralDir.code.code == 403 || responseMvnCentralDir.code.code == 504) && responseMvnCentralDir.headers.exists(header => header.name == "Proxy-Connection" && header.value == "Keep-Alive"))
-      )
-
-      println(responseMvnCentralDir.code.code)
-
-
-      println("maven central checks:")
-      checks
-        .map { case (prompt, passed) => if(passed) s"$prompt: X" else s"$prompt: O" }.mkString(
-          "\t",
-          "\n\t",
-          ""
-        )
-        .pipe(println)
-    }
-    catch {
-      case ce: SttpClientException.ConnectException =>
-        println("Couldn't connect to maven central, maybe you're offline?")
-    }
   }
 
   private def checkIsNativeOrJvm(): Unit = {
